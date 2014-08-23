09b32e5687ce69b012843a08711ffe8d7b7049e6
chiefpatato <cysgmaverick@gmail.com>
2014-08-23 11:10:08 -0400

    BADGERS
    badgers patatos!!!

---
+++ BADGERS!.py
@@ -0,0 +1,34 @@
+import time
+
+def badgers():
+    badgers = 1
+    while badgers <= 12:
+        print("Badgers! ")
+        badgers= badgers + 1
+        time.sleep(0.3)
+        
+
+def mushroom():
+    mushroom = 1
+    while mushroom <= 4:
+         print("Mushroom! ")
+         mushroom= mushroom + 1
+         time.sleep(0.5)
+
+
+def snake():
+    snake = 1
+    while snake <= 3:
+        print("Snake! ")
+        print("Ohhhhhh! ")
+        snake= snake + 1
+        time.sleep(0.9)
+    
+
+#############################################
+
+badgers()
+
+mushroom()
+
+snake()
