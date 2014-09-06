529409ecd01a9935e4cb3a0c39d1ad1f234154b6
chiefpatato <cysgmaverick@gmail.com>
2014-09-06 10:49:39 -0400

    8 BALL 2
    HI

---
+++ MAGIC 8 BALL 2.py
@@ -0,0 +1,19 @@
+import random
+
+print("Behold, I am the magic 8 Ball! ")
+
+while True:
+    
+    print("Type your question below.")
+    question = input('> ')
+    answer = random.choice(['Yes','No','Mabye',])
+    print(answer)
+
+
+
+   
+
+    
+
+
+     
